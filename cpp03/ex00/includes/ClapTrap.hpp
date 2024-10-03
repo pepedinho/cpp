18562c8d99636d6b0f78a8e75e360a304dcec103
pepedinho <2spii94@gmail.com>
2024-10-03 18:08:45 +0200

    cpp03/ex00 OK

---
+++ cpp03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 16:10:24 by itahri            #+#    #+#             */
+/*   Updated: 2024/10/03 16:18:54 by itahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+  private:
+    std::string name;
+    int         hitPoint;
+    int         energyPoint;
+    int         attackDamage;
+  public:
+    ClapTrap(std::string name);
+    ~ClapTrap(void);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+# endif
