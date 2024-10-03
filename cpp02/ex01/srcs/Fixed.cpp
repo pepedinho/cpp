3c10371b495e9ee9352c886b981e6db5b6bc6a7e
pepedinho <2spii94@gmail.com>
2024-10-03 13:27:01 +0200

    cpp02/ex01 in progress

---
+++ cpp02/ex01/srcs/Fixed.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 11:35:58 by itahri            #+#    #+#             */
+/*   Updated: 2024/10/01 12:00:32 by itahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+// Constructor
+Fixed::Fixed() {
+  std::cout << "Default constructor called" << std::endl;
+  value = 0;
+};
+
+// Copy constructor
+Fixed::Fixed(const Fixed& src) {
+  std::cout << "Copy constructor called" << std::endl;
+  *this = src;
+}
+
+// Surcharge d'operateur d'affectation
+Fixed& Fixed::operator=(const Fixed& other) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &other)
+    this->value = other.getRawBits();
+  return *this;
+}
+
+// Destructor
+Fixed::~Fixed(void) {
+  std::cout << "Destructor called" << std::endl;
+}
+
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return this->value;
+}
+
+void Fixed::setRawBits(int const raw) {
+  this->value = raw;
+}
