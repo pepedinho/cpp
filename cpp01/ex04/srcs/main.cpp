3eed4af99bbc2ca43aaba29f404fd8aa2c466e12
pepedinho <2spii94@gmail.com>
2024-10-18 10:46:46 +0200

    few changements 4defense

--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -14,12 +14,14 @@
 
 int	main(int argc, char *argv[])
 {
-  if (argc != 4) {
-    std::cerr << "To much args <filename> <s1> <s2> " << std::endl;
+  if (argc > 4) {
+    std::cerr << "To much args : <filename> <s1> <s2> " << std::endl;
+    return 1;
+  } else if (argc < 4) {
+    std::cerr << "Not enough args : <filename> <s1> <s2> " << std::endl;
     return 1;
   }
   File *file = new File(argv[1], argv[2], argv[3]);
-
   file->replace();
 	return 0;
 }
