9de470933ccb43a4bae5b498919d84fc368e5a87
pepedinho <2spii94@gmail.com>
2024-10-30 16:57:59 +0100

    cpp05 ex02 wip

---
+++ cpp05/ex02/includes/RobotomyRequestForm.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 16:26:46 by itahri            #+#    #+#             */
+/*   Updated: 2024/10/30 16:29:06 by itahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include <iostream>
+#include <string>
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm
+{
+    private:
+      std::string _target;
+    public:
+        RobotomyRequestForm();
+        RobotomyRequestForm(std::string target);
+        RobotomyRequestForm(RobotomyRequestForm const & src);
+        ~RobotomyRequestForm();
+        RobotomyRequestForm & operator=(RobotomyRequestForm const & src);
+
+        void execute(const Bureaucrat & executor) const;
+};
+
+#endif 
