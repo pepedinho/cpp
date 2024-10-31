451059a24188122a20b49c5ac46d57093e003fe0
pepedinho <2spii94@gmail.com>
2024-10-31 18:14:21 +0100

    cpp06/ex00 OK

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:28:22 by itahri            #+#    #+#             */
-/*   Updated: 2024/10/31 17:32:59 by itahri           ###   ########.fr       */
+/*   Updated: 2024/10/31 18:11:43 by itahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,39 @@
 #include <cstdlib>
 #include <variant>
 
-bool isInt(std::string nbr)
-{
-  int cnt = 0;
+/*bool isInt(std::string nbr)*/
+/*{*/
+/*  int cnt = 0;*/
+/**/
+/*  for (int i = 0; nbr[i]; i++) {*/
+/*    if ((nbr[i] < '0' || nbr[i] > '9') && nbr[i] != 'f')*/
+/*      return false;*/
+/*    if (nbr[i] == 'f')*/
+/*      cnt = 1;*/
+/*    if (cnt > 1)*/
+/*      return false;*/
+/*  }*/
+/*  return true;*/
+/*}*/
 
-  for (int i = 0; nbr[i]; i++) {
-    if ((nbr[i] < '0' || nbr[i] > '9') && nbr[i] != 'f')
-      return false;
-    if (nbr[i] == 'f')
-      cnt = 1;
-    if (cnt > 1)
-      return false;
-  }
-  return true;
+bool valueCheck(std::string str) {
+  if (str != "+inf" && str != "-inf" && str != "nan") 
+		return false;
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	if (str == "inf" || str == "+inf") {
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+	}
+	else if (str == "-inf") {
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+	}
+	if (str == "nan") {
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	return true;
 }
 
 void printFloat(float nb) {
@@ -46,7 +66,8 @@ void printDouble(double nb) {
 }
 
 void printInt(int nb) {
-  
+  std::cout << "int: ";
+  std::cout << nb << std::endl;
 }
 
 void printChar(char c) {
@@ -65,14 +86,54 @@ void floatCase(std::string to_convert) {
   buf >> nb;
   if (buf.fail())
     std::cout << "Error" << std::endl;
+  else {
+    printChar(static_cast<char>(nb));
+    printInt(static_cast<int>(nb));
+    printFloat(nb);
+    printDouble(static_cast<double>(nb));
+  }
+}
+
+void intCase(std::string to_convert) {
+  std::stringstream buf;
+  int nb;
+  buf << to_convert;
+  buf >> nb;
+  if (buf.fail())
+    std::cout << "Error" << std::endl;
+  else {
+    printChar(static_cast<char>(nb));
+    printInt(nb);
+    printFloat(static_cast<float>(nb));
+    printDouble(static_cast<double>(nb));
+  }
+}
+
+void doubleCase(std::string to_convert) {
+  std::stringstream buf;
+  double nb;
+  buf << to_convert;
+  buf >> nb;
+  if (buf.fail())
+    std::cout << "Error" << std::endl;
+  else {
+    printChar(static_cast<char>(nb));
+    printInt(static_cast<int>(nb));
+    printFloat(static_cast<float>(nb));
+    printDouble(nb);
+  }
 }
 
 void ScalarConverter::convert(std::string to_convert) {
-  if (!isInt(to_convert))
-    std::cout << "Error" << std::endl;
-  if (to_convert.find("f") != std::string::npos) {
-
+  /*if (!isInt(to_convert))*/
+  /*  std::cout << "Error" << std::endl;*/
+  if (valueCheck(to_convert)) {
+    return;
+  } else if (to_convert.find("f") != std::string::npos) {
+    floatCase(to_convert);
   } else if (to_convert.find(".") != std::string::npos) {
-
+    doubleCase(to_convert);
+  } else {
+    intCase(to_convert);
   }
 }
--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -6,12 +6,21 @@
 /*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:58:05 by itahri            #+#    #+#             */
-/*   Updated: 2024/10/31 14:58:59 by itahri           ###   ########.fr       */
+/*   Updated: 2024/10/31 18:10:37 by itahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main (int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
-  return 0;
+#include "../includes/ScalarConverter.hpp"
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "I need 1 argument to convert !! " << std::endl;
+		return 1;
+	}
+	if (argc > 2 ) {
+		std::cout << "I need only 1 argument to convert !! " << std::endl;
+		return 1;
+	}
+	ScalarConverter::convert(argv[1]);
+	return 0;
 }
