6b803a207380bd0b0fb999a0205eeda557f21cfe
pepedinho <2spii94@gmail.com>
2025-04-22 20:33:11 +0200

    start cpp09/ex02

---
+++ cpp09/ex02/includes/Jhonson.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Jhonson.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itahri <itahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 17:02:04 by itahri            #+#    #+#             */
+/*   Updated: 2025/04/22 17:07:44 by itahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef JHONSON_CPP
+#define JHONSON_CPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+class Jhonson {
+  private: 
+    std::vector<int> _big;
+    std::vector<int> _small;
+
+  public:
+    Jhonson();
+};
+
+
+#endif
